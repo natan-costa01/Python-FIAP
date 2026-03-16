d5a05f1572196816b2bd626903033f0483d64f41
natan-costa01 <silvacnatan@gmail.com>
2026-03-16 20:46:59 -0300

    Primeira aula prática de python.

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,21 @@
+print("Olá, mundo!")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4")
+
+'''
+Comentários de multiplas linhas
+'''
+nome = "Natan"
+idade = 18
+peso = 68.7
+
+print(nome, idade, peso)
+print(f"Olá, {nome}!")
+
+#inputs -------
+
+nome = input("Digite seu nome: ")
+print(f"Seja bem-vindo(a) {nome}")
+
---
+++ aula02-variaveis-operadores/desafio 1,2,3.py
@@ -0,0 +1,15 @@
+nome = input("Digite seu nome: ")
+print(f"Seja bem-vindo(a) {nome}")
+
+dia = input("Digite o dia do seu nascimento: ")
+mes = input("Digite o mês do seu nascimento: ")
+ano = input("Digite o ano do seu nascimento: ")
+
+print(f"Olá, {nome}. Você nasceu na data {dia}/{mes}/{ano}!")
+
+n1 = input("Digite um número: ")
+n2 = input("Digite outro número: ")
+
+soma = float(n1) + float(n2)
+
+print(soma)
