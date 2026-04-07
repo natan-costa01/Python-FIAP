a1e32a90adac58f8b13eb7e7d2e56f90d1340d35
natan-costa01 <silvacnatan@gmail.com>
2026-04-06 21:33:02 -0300

    Material da aula e exercicios.

--- aula03/exercicios/ex06.py
+++ aula03/exercicios/ex06.py
@@ -1,7 +1,21 @@
 n1 = int(input("Digite um número inteiro: "))
 n2 = int(input("Digite outro número inteiro: "))
-n2 = input("Digite uma operação matematica (/, *, +, -) : "))
+operação = input("Digite uma operação matematica (/, *, +, -) : ")
 
-if
+faz = 0
 
+if operação == "/":
+    faz = n1 / n2
+    print(f"A divisão é: {faz}")
+elif operação == "*":
+    faz = n1 * n2
+    print(f"A multiplicação é: {faz}")
+elif operação == "+":
+    faz = n1 + n2
+    print(f"A soma é: {faz}")
+elif operação == "-":
+    faz = n1 - n2
+    print(f"A subtração é: {faz}")
+else:
+    print("Digite um valor valido")
 
